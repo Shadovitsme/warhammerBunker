7dcd8df4545e019413a61b1b5432f13c48a3113a
ukki <iu.cashirskaia@yandex.ru>
2025-04-09 01:44:38 +0300

    катастрофы и немного оформления

--- src/components/itemCard.vue
+++ src/components/itemCard.vue
@@ -6,8 +6,10 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
-    <p>{{ props.head }}</p>
-    <p>{{ props.text }}</p>
+  <div
+    class="text-center p-4 hover:w-50 w-40 text-amber-950 rounded-2xl bg-amber-500 duration-200"
+  >
+    <p class="">{{ props.head }}</p>
+    <p class="mt-4 text-xl font-bold">{{ props.text }}</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
-<template>
-  <div class="">
-    <button @click="clickCount++">add item</button>
-    <ItemCard text="профессия"></ItemCard>
-    <p v-if="clickCount > 0">профессия</p>
-    <p v-if="clickCount > 1">здоровье</p>
-    <p v-if="clickCount > 2">факт</p>
-    <p v-if="clickCount > 3">хобби</p>
-    <p v-if="clickCount > 4">фобия</p>
-    <p v-if="clickCount > 5">багаж</p>
-  </div>
-</template>
-
 <script setup>
+import catastroph from "@/bunkerArrays/catastroph";
 import ItemCard from "@/components/itemCard.vue";
 import { ref } from "vue";
 const clickCount = ref(0);
+const generateCatastroph = ref(false);
+let catastrophText = ref("");
+
+function catastrophRegenerate() {
+  catastrophText.value = catastroph();
+  generateCatastroph.value = true;
+}
 </script>
+<template>
+  <div>
+    <div class="w-full justify-center flex">
+      <button
+        class="rounded-3xl bg-red-800 hover:bg-red-700 active:bg-red-900 duration-500 py-3 px-5 mb-4 text-red-50 hover:text-red-100 text-2xl hover:animate-pulse font-bold"
+        @click="catastrophRegenerate"
+      >
+        Катастрофа!
+      </button>
+    </div>
+    <p
+      v-if="generateCatastroph"
+      class="animate-bounce w-full text-center text-7xl my-20 text-red-700"
+    >
+      {{ catastrophText }}
+    </p>
+
+    <div class="w-full justify-center flex">
+      <button
+        class="rounded-3xl bg-red-800 hover:bg-red-700 active:bg-red-900 duration-500 py-3 px-5 mb-4 text-red-50 hover:text-red-100 hover:text-2xl font-bold"
+        @click="clickCount++"
+      >
+        Тянуть карту!
+      </button>
+    </div>
+
+    <div class="flex gap-5 transition-all duration-300 items-center">
+      <div v-if="clickCount > 0" class="transition-all duration-300">
+        <ItemCard head="профессия" text="профессия"></ItemCard>
+      </div>
+      <div v-if="clickCount > 1" class="transition-all duration-300">
+        <ItemCard head="здоровье" text="профессия"></ItemCard>
+      </div>
+      <div v-if="clickCount > 2" class="transition-all duration-300">
+        <ItemCard head="факт" text="профессия"></ItemCard>
+      </div>
+      <div v-if="clickCount > 3" class="transition-all duration-300">
+        <ItemCard head="хобби" text="профессия"></ItemCard>
+      </div>
+      <div v-if="clickCount > 4" class="transition-all duration-300">
+        <ItemCard head="фобия" text="профессия"></ItemCard>
+      </div>
+      <div v-if="clickCount > 5" class="transition-all duration-300">
+        <ItemCard head="багаж" text="профессия"></ItemCard>
+      </div>
+    </div>
+  </div>
+</template>
