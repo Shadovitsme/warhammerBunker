d0dbe97030df78eb4217935ca036f9bf62e3bfe9
ukki <iu.cashirskaia@yandex.ru>
2025-04-27 01:21:03 +0300

    примитивные формулы

--- src/components/PiuPiuComponents/roadComponent.vue
+++ src/components/PiuPiuComponents/roadComponent.vue
@@ -1,8 +1,20 @@
 <script setup>
-const props = defineProps({ playerHere: { type: Boolean, required: true } });
+const props = defineProps({
+  playerHere: { type: Boolean, required: true },
+  formula: { type: String, required: true },
+  number: { type: String },
+});
 </script>
 <template>
-  <div class="py-10 h-full w-1/4 bg-red-100 flex justify-center items-end">
-    <div v-if="props.playerHere" class="bg-black size-20"></div>
+  <div
+    class="py-10 h-full w-1/4 bg-red-100 flex relative justify-center items-end"
+  >
+    <p class="absolute top-10">{{ props.formula }}</p>
+    <div
+      v-if="props.playerHere"
+      class="bg-black size-20 absolute bottom-10 flex"
+    >
+      <p class="m-auto text-white text-4xl">{{ props.number }}</p>
+    </div>
   </div>
 </template>
--- src/views/PiuView.vue
+++ src/views/PiuView.vue
@@ -2,7 +2,24 @@
 import RoadComponent from "@/components/PiuPiuComponents/roadComponent.vue";
 import { ref } from "vue";
 
-const playerHere = ref(0);
+const playerHere = ref(1);
+const number = ref(1);
+const roadCounter = ref(2);
+
+let formulaArray = [];
+
+for (let i = 1; i < roadCounter.value + 1; i++) {
+  formulaArray.push({
+    formulaText: "+" + i,
+    formulaAction: "+",
+    formulaNum: i,
+  });
+}
+
+function chooseRoad(roadNum) {
+  playerHere.value = roadNum;
+  number.value = number.value + formulaArray[roadNum - 1].formulaNum;
+}
 </script>
 
 <template>
@@ -10,12 +27,12 @@ const playerHere = ref(0);
     class="top-0 left-0 absolute right-0 bottom-0 flex justify-center items-center gap-1"
   >
     <RoadComponent
-      @click="playerHere = 0"
-      :player-here="playerHere == 0"
-    ></RoadComponent>
-    <RoadComponent
-      @click="playerHere = 1"
-      :player-here="playerHere == 1"
+      v-for="i in roadCounter"
+      :key="i"
+      @click="chooseRoad(i)"
+      :formula="formulaArray[i - 1].formulaText"
+      :player-here="playerHere == i"
+      :number="number"
     ></RoadComponent>
   </div>
 </template>
